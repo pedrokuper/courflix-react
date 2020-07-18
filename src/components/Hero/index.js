e33eb41d08e7ff5ea76e7d973487eccb427656d4
Pedro <pkupersmit@gmail.com>
2020-07-18 05:00:48 -0300

    hero section done

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,25 +1,26 @@
 import React from "react";
+import "./style.scss";
 
 class Hero extends React.Component {
   render() {
     return (
-      <div className="hero-wrapper">
-        <section className="hero" id="inicio">
-          <div className="content">
+      <div className="hero--wrapper">
+        <section className="hero--section" id="inicio">
+          <div className="hero--content">
             <h2 className="hero--subtitle">Original de COURFLIX</h2>
             <h1 className="hero--title">BLACK SUMMER</h1>
             <h3 className="hero--mobile">2019 - 16+ - 1 temporada</h3>
-            <div className="hero-buttons">
-              <a href="#" className="hero-button-link play">
+            <div className="hero--buttons">
+              <a href="#" className="hero-button play">
                 Reproducir
               </a>
-              <a href="#" className="hero-button-link my-list">
+              <a href="#" className="hero-button my-list">
                 + Mi lista
               </a>
             </div>
-            <div className="hero-description">
-              <p className="hero-text">Ve la temporada 1</p>
-              <p className="hero-description">
+            <div className="hero--description">
+              <p className="hero--text">Ve la temporada 1</p>
+              <p className="hero--plot">
                 Una ansiada luna de miel. Un asesinato terrible.Varios
                 sospechosos. Si sobreviven, serán unas vacaciones de ensueño.
               </p>
