5d9333625c01de676aebfa242238ed8cf2c97e31
Pedro <pkupersmit@gmail.com>
2020-11-25 22:49:45 -0300

    burger custom

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,41 @@
 import React from "react";
-
+import BurgerMenu from "../BurgerMenu";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo/courflix-logo.png";
 import "./style.scss";
 
 class Navbar extends React.Component {
- 
   render() {
     const { showNav, id } = this.props;
-
+    console.log(window.innerWidth);
     return (
-      <>
-
-      </>
+      <div className="navbar-wrapper">
+        <Link to="/">
+          <div id={id}></div>
+          <img
+            src={logo}
+            className="courflix-logo"
+            alt="Logo de Courflix"
+          ></img>
+        </Link>
+        <BurgerMenu />
+        {showNav && (
+          <nav className="navbar">
+            <ul className="navbar-list">
+              <a href="#inicio">
+                <li className="navbar-link">Inicio</li>
+              </a>
+              <a href="#series">
+                <li className="navbar-link">Series</li>
+              </a>
+              <li className="navbar-link">Películas</li>
+              <a href="#last-added">
+                <li className="navbar-link">Agregados recientemente</li>
+              </a>
+            </ul>
+          </nav>
+        )}
+      </div>
     );
   }
 }
