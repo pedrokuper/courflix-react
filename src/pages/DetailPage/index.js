5cce38fb1ddcb80f316a31ffb14f87a0f45d4fbc
Pedro <pkupersmit@gmail.com>
2020-12-18 18:26:38 -0300

    deleted myList component. Not used. Deleted unused code in details page

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import content from "../../data/content.json";
-import "./style.scss";
 import Hero from "../../components/Hero";
 import Carousel from "../../components/Carousel";
 import Footer from "../../components/Footer";
